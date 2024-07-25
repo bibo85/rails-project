9fa707a35e5a935238ac213c2050fdce76215441
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-07-25 16:59:46 +0500

    Added check uniqueness name field at the current level

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,14 +3,13 @@
 class Page < ApplicationRecord
   has_ancestry
 
-  # belongs_to :parent, class_name: "Page", optional: true
-  # has_many :children, class_name: "Page", foreign_key: "parent_id", dependent: :destroy
-
   validates :name, presence: true, format: {
     with: /\A[a-zA-Zа-яА-я_]+\z/,
     message: "Доступны только английские и русские буквы, а также знак нижнего подчеркивания"
   }
   validates :title, presence: true, length: {minimum: 2, maximum: 150}
   validates :body, presence: true, length: {minimum: 2}
+
+  validates :name, uniqueness: {scope: [:ancestry], message: "- поле уже используется на этом уровне вложенности"}
 
 end
