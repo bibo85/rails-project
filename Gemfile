db7adaf0967d8605cd5cf1f655926265c183579b
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-07-29 19:20:14 +0500

    Add tests for Pages controller

--- Gemfile
+++ Gemfile
@@ -47,11 +47,13 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
+  gem "factory_bot_rails"
 end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem "rubocop", "~> 0.72.0"
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
@@ -64,6 +66,8 @@ group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
+  gem "rspec-rails", "~> 6.1"
+  gem "rails-controller-testing"
 end
 
 # Bootstrap
---
+++ spec/controllers/pages/index_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :controller do
+  describe 'GET index' do
+    # проверка ответа 200
+    it 'return http success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    # проверка корректности используемого шаблона
+    it 'used correct template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    # проверка контекста страницы
+    it 'assigns correctly context @pages' do
+      pages = %w[name_first name_second name_third].map do |name|
+        FactoryBot.create(:page, name: name, title: "#{name}_title", body: "#{name}_body")
+      end
+
+      get :index
+      expect(assigns(:pages)).to eq(pages)
+    end
+  end
+end
