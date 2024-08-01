7aaf5dd48416334a6e998329934044c94b1dfc96
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-08-01 12:19:03 +0500

    Refactoring tests for the index page

--- spec/controllers/pages/index_spec.rb
+++ spec/controllers/pages/index_spec.rb
@@ -4,26 +4,25 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
   describe 'GET index' do
-    # проверка ответа 200
+
+    let!(:page1) {create(:pages)}
+    let!(:page2) {create(:pages)}
+    let!(:page3) {create(:pages, parent_id: '1')}
+    let!(:page4) {create(:pages, parent_id: '3')}
+
     it 'return http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
 
-    # проверка корректности используемого шаблона
     it 'used correct template' do
       get :index
       expect(response).to render_template(:index)
     end
 
-    # проверка контекста страницы
     it 'assigns correctly context @pages' do
-      pages = %w[name_first name_second name_third].map do |name|
-        FactoryBot.create(:page, name:, title: "#{name}_title", body: "#{name}_body")
-      end
-
       get :index
-      expect(assigns(:pages)).to eq(pages)
+      expect(assigns(:pages)).to eq([page1, page2])
     end
   end
 end
