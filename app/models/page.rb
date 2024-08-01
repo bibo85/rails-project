df1157a35a75395704fb02409217fd4e76f09403
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-08-01 12:36:15 +0500

    Del validation of the length of the title and body fields

--- app/models/page.rb
+++ app/models/page.rb
@@ -8,8 +8,8 @@ class Page < ApplicationRecord
     with: /\A[a-zA-Zа-яА-я_]+\z/,
     message: '- в поле доступны только английские и русские буквы, а также знак нижнего подчеркивания'
   }
-  validates :title, presence: true, length: { minimum: 2, maximum: 150 }
-  validates :body, presence: true, length: { minimum: 2 }
+  validates :title, presence: true
+  validates :body, presence: true
 
   validates :name, uniqueness: { scope: [:ancestry], message: '- поле уже используется на этом уровне вложенности' }
 end
