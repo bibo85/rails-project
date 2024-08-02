a87045751c7956fc9debaec877740e149bba6976
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2024-08-02 19:04:40 +0500

    Add tests for page model

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,3 +1,31 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+RSpec.describe Page, type: :model do
+
+  describe 'validations' do
+
+    it ''
+
+    context 'unique page names at the same nesting level relative to the parent' do
+
+      let!(:page1) {create(:pages)}
+
+      it 'is valid' do
+        page2 = create(:pages)
+        expect(page2).to be_valid
+      end
+
+      it 'is invalid' do
+        page2 = create(:pages, name: "test_page", parent_id: page1.id)
+        page3 = build(:pages, name: "test_page", parent_id: page1.id)
+        expect(page3).not_to be_valid
+
+      end
+
+    end
+
+  end
+
+end
